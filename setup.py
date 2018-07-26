e532b19492cde594b5c537297564ecc7ca87063c
Thomas Yu <thomasyu888@gmail.com>
2018-07-26 15:12:44 -0700

    Add in setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='aacrgenie',
+      version='1.0.5',
+      description='Processing and validation for GENIE',
+      url='https://github.com/Sage-Bionetworks/Genie',
+      author='Thomas Yu',
+      author_email='thomasyu888@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      entry_points = {
+        'console_scripts': ['genie = processing.__main__:main']},
+      install_requires=[
+        'pandas>=0.20.0',
+        'synapseclient',
+        'httplib2',
+        'pycrypto'])
