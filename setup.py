d74eb7ef7a3a377c2a9df98f78cf99a134c1e582
Thomas Yu <thomasyu888@gmail.com>
2018-08-29 14:39:30 -0700

    Push 1.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='aacrgenie',
-      version='1.3.0',
+      version='1.4.0',
       description='Processing and validation for GENIE',
       url='https://github.com/Sage-Bionetworks/Genie',
       author='Thomas Yu',
