1f01a2ba4251fed0f8c612939c03e5fa9b8d18c7
thomasyu888 <thomasyu888@gmail.com>
2018-09-18 23:17:30 -0700

    Allow for redaction values in vital status, merge in vital status values, add in column to check if column is used in clinical db

--- genie/vitalStatus.py
+++ genie/vitalStatus.py
@@ -55,16 +55,17 @@ class vitalStatus(example_filetype_format.FileTypeFormat):
 		#INT CONTACT
 		haveColumn = process_functions.checkColExist(vitalStatusDf, "INT_CONTACT")
 		if haveColumn:
-			if not all([process_functions.checkInt(i) for i in vitalStatusDf.INT_CONTACT if not pd.isnull(i) and not in ['>32485','<6570']]):
-				total_error += "Vital status file: Please double check your INT_CONTACT column, it must be an integer.\n"
+			#notNullContact = vitalStatusDf.INT_CONTACT[~vitalStatusDf.INT_CONTACT.isnull()]
+			if not all([process_functions.checkInt(i) for i in vitalStatusDf.INT_CONTACT if not pd.isnull(i) and i not in ['>32485','<6570']]):
+				total_error += "Vital status file: Please double check your INT_CONTACT column, it must be an integer, NA/null/empty, >32485, or <6570.\n"
 		else:
 			total_error += "Vital status file: Must have INT_CONTACT column.\n"
 
 		#INT DOD
 		haveColumn = process_functions.checkColExist(vitalStatusDf, "INT_DOD")
 		if haveColumn:
-			if not all([process_functions.checkInt(i) for i in vitalStatusDf.INT_DOD if not pd.isnull(i) and not in ['>32485','<6570']]):
-				total_error += "Vital status file: Please double check your INT_DOD column, it must be an integer or NA/null/empty.\n"
+			if not all([process_functions.checkInt(i) for i in vitalStatusDf.INT_DOD if not pd.isnull(i) and i not in ['>32485','<6570']]):
+				total_error += "Vital status file: Please double check your INT_DOD column, it must be an integer, NA/null/empty, >32485, or <6570.\n"
 		else:
 			total_error += "Vital status file: Must have INT_DOD column.\n"
 
