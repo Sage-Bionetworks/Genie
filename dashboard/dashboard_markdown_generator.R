5e1c2b19d5f2c5e852ed4846a25db903902a6886
Thomas Yu <thomasyu888@gmail.com>
2018-11-29 14:11:42 -0800

    Allow for testing of dashboard script

--- dashboard/dashboard_markdown_generator.R
+++ dashboard/dashboard_markdown_generator.R
@@ -3,6 +3,7 @@ suppressPackageStartupMessages(library(argparse))
 parser <- ArgumentParser()
 parser$add_argument("release", help="Release version (ie. 5.3-consortium)")
 parser$add_argument("--staging", action="store_true", help="Use staging files")
+parser$add_argument("--testing", action="store_true", help="Use testing files")
 parser$add_argument("--syn_user", help="Synapse username")
 parser$add_argument("--syn_pass", help="Synapse password")
 
@@ -53,19 +54,26 @@ tryCatch({
 })
 #options(bitmapType='cairo')
 
-#if (args$staging) {
-#  databaseSynIdMappingId = 'syn12094210'
-#} else{
-#  databaseSynIdMappingId = 'syn10967259'
-#}
+if (args$staging) {
+ database_synid_mappingid = 'syn12094210'
+} else if (args$testing) {
+ database_synid_mappingid = 'syn11600968'
+} else{
+ database_synid_mappingid = 'syn10967259'
+}
 
 #Should use this to create a staging version of the dashboard
-#databaseSynIdMapping = synTableQuery(sprintf('select * from %s', databaseSynIdMappingId))
-#databaseSynIdMappingDf = as.data.frame(databaseSynIdMapping)
-#mainReleaseFolderSynId = databaseSynIdMappingDf$Id[databaseSynIdMappingDf$Database == "release"]
+database_synid_mapping = synTableQuery(sprintf('select * from %s', database_synid_mappingid))
+database_synid_mappingdf = as.data.frame(database_synid_mapping)
+release_folder_fileview_synid = database_synid_mappingdf$Id[database_synid_mappingdf$Database == "releaseFolder"]
 
+choose_from_release = synTableQuery(paste(sprintf("select distinct(name) as releases from %s", release_folder_fileview_synid),"where name not like 'Release%' and name <> 'case_lists'"))
+releases = as.data.frame(choose_from_release)
+if (!any(releases$releases %in% release)) {
+  stop(sprintf("Must choose correct release: %s", paste0(releases$releases, collapse=", ")))
+}
 
-release_folder_fileview_synid = "syn17019650"
+#release_folder_fileview_synid = "syn17019650"
 release_folder = synTableQuery(sprintf("select id from %s where name = '%s'", release_folder_fileview_synid, release))
 release_folder_synid = release_folder$asDataFrame()$id
 template = readLines("dashboardTemplate.Rmd")
