2b1a404e8ee8da08a62c31a3436d6f053ee819aa
Thomas Yu <thomasyu888@gmail.com>
2018-12-27 14:12:43 -0800

    Add documentation to example filetype format

--- genie/example_filetype_format.py
+++ genie/example_filetype_format.py
@@ -20,6 +20,12 @@ class FileTypeFormat(object):
 		'''
 		This function by defaults assumes the filePathList is length of 1 
 		and is a tsv file.  Could change depending on file type.
+		
+		Args:
+			filePathList:  A list of file paths (Max is 2 for the two clinical files)
+
+		Returns:
+			df: Pandas dataframe of file
 		'''
 		filePath = filePathList[0]
 		df = pd.read_csv(filePath,sep="\t",comment="#")
@@ -29,31 +35,70 @@ class FileTypeFormat(object):
 		'''
 		Each file is to be read in for validation and processing.
 		This is not to be changed in any functions.
+
+		Args:
+			filePathList:  A list of file paths (Max is 2 for the two clinical files)
+		
+		Returns:
+			df: Pandas dataframe of file
 		'''
 		df = self._get_dataframe(filePathList)
 		return(df)
 
 	def _validateFilename(self, filePath):
+		'''
+		Function that changes per file type for validating its filename
+
+		Args:
+			filePath: Path to file
+		'''
 		pass
 
 
 	def validateFilename(self, filePath):
+		'''
+		Validation of file name.  The filename is what maps the file
+		to its validation and processing.
+
+		Args:
+			filePath: Path to file
+
+		Returns:
+			str: file type defined by self._fileType
+		'''
 		self._validateFilename(filePath)
 		return(self._fileType)
 
 
 
 	def process_steps(self, df, **kwargs):
+		'''
+		This function is modified for every single file.  
+		It reformats the file and stores the file into database and Synapse.
+		'''
 		pass
 
 	def preprocess(self, filePath, **kwargs):
-# - clinical
-# - maf
-# - vcf
+		'''
+		This is for any preprocessing that has to occur to obtain kwargs for processing.
+
+		Args:
+			filePath: Path to file
+		'''
 		return(dict())
 
 
 	def process(self, filePath, **kwargs):
+		'''
+		This is the main processing function.
+
+		Args:
+			filePath: Path to file
+			kwargs: The kwargs are determined by self._process_kwargs
+
+		Returns:
+			str: file path of processed file
+		'''
 		preprocess_args = self.preprocess(filePath, **kwargs)
 		kwargs.update(preprocess_args)
 		mykwargs = {}
@@ -70,12 +115,34 @@ class FileTypeFormat(object):
 		return(path)
 
 	def _validate(self, df, **kwargs):
-		total_error =""
+		'''
+		This is the base validation function. 
+		By default, no validation occurs.
+
+		Args:
+			df: A dataframe of the file
+			kwargs: The kwargs are determined by self._validation_kwargs
+
+		Returns:
+			tuple: The errors and warnings as a file from validation. Defaults to blank strings
+		'''
+		total_error = ""
 		warning = ""
 		logger.info("NO VALIDATION for %s files" % self._fileType)
 		return(total_error, warning)
 
 	def validate(self, filePathList, **kwargs):
+		'''
+		This is the main validation function. 
+		Every file type calls self._validate, which is different.
+
+		Args:
+			filePathList: A list of file paths.
+			kwargs: The kwargs are determined by self._validation_kwargs
+
+		Returns:
+			tuple: The errors and warnings as a file from validation.
+		'''
 		mykwargs = {}
 		for required_parameter in self._validation_kwargs:
 			assert required_parameter in kwargs.keys(), "%s not in parameter list" % required_parameter
