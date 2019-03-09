a5323724ddc8f65a36eff781b9eb6f14025a3fa7
thomasyu888 <thomasyu888@gmail.com>
2019-03-09 12:27:27 -0800

    convert tabs to spaces

--- genie/assay.py
+++ genie/assay.py
@@ -9,112 +9,112 @@ logger = logging.getLogger(__name__)
 
 
 class Assayinfo(example_filetype_format.FileTypeFormat):
-	'''
-	Assay information file type
-	'''
-	_fileType = "assayinfo"
+    '''
+    Assay information file type
+    '''
+    _fileType = "assayinfo"
 
-	_process_kwargs = ["newPath", "databaseSynId"]
+    _process_kwargs = ["newPath", "databaseSynId"]
 
-	def _validateFilename(self, filepath_list):
-		assert os.path.basename(filepath_list[0]) == "assay_information.yaml"
+    def _validateFilename(self, filepath_list):
+        assert os.path.basename(filepath_list[0]) == "assay_information.yaml"
 
-	def process_steps(self, filePath, newPath, databaseSynId):
-		logger.info('PROCESSING %s' % filePath)
-		#databaseSynId = kwargs['databaseSynId']
-		#Must pass in a list
-		assay_info_df = self._get_dataframe([filePath])
-		process_assay_info_df = self._process(assay_info_df)
-		process_functions.updateData(self.syn, databaseSynId, process_assay_info_df, self.center, filterByColumn= "CENTER", toDelete=True)
-		return(filePath)
+    def process_steps(self, filePath, newPath, databaseSynId):
+        logger.info('PROCESSING %s' % filePath)
+        #databaseSynId = kwargs['databaseSynId']
+        #Must pass in a list
+        assay_info_df = self._get_dataframe([filePath])
+        process_assay_info_df = self._process(assay_info_df)
+        process_functions.updateData(self.syn, databaseSynId, process_assay_info_df, self.center, filterByColumn= "CENTER", toDelete=True)
+        return(filePath)
 
-	def _process(self, df):
-		seq_assay_ids = [assay.upper().replace('_','-') for assay in df['SEQ_ASSAY_ID']]
-		df['SEQ_ASSAY_ID'] = seq_assay_ids 
-		if process_functions.checkColExist(df, "gene_padding"):
-			df['gene_padding'] = df['gene_padding'].fillna(10)
-			df['gene_padding'] = df['gene_padding'].astype(int)
-		else:
-			df['gene_padding'] = 10
+    def _process(self, df):
+        seq_assay_ids = [assay.upper().replace('_','-') for assay in df['SEQ_ASSAY_ID']]
+        df['SEQ_ASSAY_ID'] = seq_assay_ids 
+        if process_functions.checkColExist(df, "gene_padding"):
+            df['gene_padding'] = df['gene_padding'].fillna(10)
+            df['gene_padding'] = df['gene_padding'].astype(int)
+        else:
+            df['gene_padding'] = 10
 
-		if not process_functions.checkColExist(df, "variant_classifications"):
-			df['variant_classifications'] = pd.np.nan
+        if not process_functions.checkColExist(df, "variant_classifications"):
+            df['variant_classifications'] = pd.np.nan
 
-		df['CENTER'] = self.center
-		return(df)
-		#sequencing_center
+        df['CENTER'] = self.center
+        return(df)
+        #sequencing_center
 
-	def _get_dataframe(self, filepath_list):
-		'''
-		Takes in yaml file, returns dataframe
-		'''
-		filepath = filepath_list[0]
-		with open(filepath, 'r') as yamlfile:
-			panel_info_dict = yaml.load(yamlfile)
-		assay_info_df = pd.DataFrame(panel_info_dict)
-		assay_info_df = assay_info_df.transpose()
-		assay_info_df['SEQ_ASSAY_ID'] = assay_info_df.index
-		assay_info_df.reset_index(drop=True, inplace=True)
-		return(assay_info_df)
+    def _get_dataframe(self, filepath_list):
+        '''
+        Takes in yaml file, returns dataframe
+        '''
+        filepath = filepath_list[0]
+        with open(filepath, 'r') as yamlfile:
+            panel_info_dict = yaml.load(yamlfile)
+        assay_info_df = pd.DataFrame(panel_info_dict)
+        assay_info_df = assay_info_df.transpose()
+        assay_info_df['SEQ_ASSAY_ID'] = assay_info_df.index
+        assay_info_df.reset_index(drop=True, inplace=True)
+        return(assay_info_df)
 
-	def _validate(self, assay_info_df):
-		total_error = ""
-		warning = ""
+    def _validate(self, assay_info_df):
+        total_error = ""
+        warning = ""
 
-		if process_functions.checkColExist(assay_info_df, "SEQ_ASSAY_ID"):
-			all_seq_assays = assay_info_df.SEQ_ASSAY_ID.unique()
-			if not all([assay.startswith(self.center) for assay in all_seq_assays]):
-				total_error += "Assay_information.yaml: Please make sure your all your SEQ_ASSAY_IDs start with your center abbreviation.\n"
-		else:
-			total_error += "Assay_information.yaml: Must have SEQ_ASSAY_ID column.\n"
+        if process_functions.checkColExist(assay_info_df, "SEQ_ASSAY_ID"):
+            all_seq_assays = assay_info_df.SEQ_ASSAY_ID.unique()
+            if not all([assay.startswith(self.center) for assay in all_seq_assays]):
+                total_error += "Assay_information.yaml: Please make sure your all your SEQ_ASSAY_IDs start with your center abbreviation.\n"
+        else:
+            total_error += "Assay_information.yaml: Must have SEQ_ASSAY_ID column.\n"
 
-		warn, error = process_functions.check_col_and_values(assay_info_df, 'is_paired_end', [True, False], filename="Assay_information.yaml", required=True)
-		warning += warn
-		total_error  += error 
-		warn, error = process_functions.check_col_and_values(assay_info_df, 'library_selection', ['Hybrid Selection','PCR','Affinity Enrichment','Poly-T Enrichment','Random','rRNA Depletion','miRNA Size Fractionation','Other'], filename="Assay_information.yaml", required=True)
-		warning += warn
-		total_error  += error 
-		warn, error = process_functions.check_col_and_values(assay_info_df, 'library_strategy', ['ATAC-Seq','Bisulfite-Seq','ChIP-Seq','miRNA-Seq','RNA-Seq','Targeted Sequencing','WGS','WXS'], filename="Assay_information.yaml", required=True)
-		warning += warn
-		total_error  += error 
-		warn, error = process_functions.check_col_and_values(assay_info_df, 'platform', ['Illumina','SOLiD','LS454','Ion Torrent','Complete Genomics','PacBio','Other'], filename="Assay_information.yaml", required=True)
-		warning += warn
-		total_error  += error 
+        warn, error = process_functions.check_col_and_values(assay_info_df, 'is_paired_end', [True, False], filename="Assay_information.yaml", required=True)
+        warning += warn
+        total_error  += error 
+        warn, error = process_functions.check_col_and_values(assay_info_df, 'library_selection', ['Hybrid Selection','PCR','Affinity Enrichment','Poly-T Enrichment','Random','rRNA Depletion','miRNA Size Fractionation','Other'], filename="Assay_information.yaml", required=True)
+        warning += warn
+        total_error  += error 
+        warn, error = process_functions.check_col_and_values(assay_info_df, 'library_strategy', ['ATAC-Seq','Bisulfite-Seq','ChIP-Seq','miRNA-Seq','RNA-Seq','Targeted Sequencing','WGS','WXS'], filename="Assay_information.yaml", required=True)
+        warning += warn
+        total_error  += error 
+        warn, error = process_functions.check_col_and_values(assay_info_df, 'platform', ['Illumina','SOLiD','LS454','Ion Torrent','Complete Genomics','PacBio','Other'], filename="Assay_information.yaml", required=True)
+        warning += warn
+        total_error  += error 
 
-		instrument_model = ['454 GS FLX Titanium','AB SOLiD 4','AB SOLiD 2','AB SOLiD 3','Complete Genomics','Illumina HiSeq X Ten',
-				'Illumina HiSeq X Five','Illumina Genome Analyzer II','Illumina Genome Analyzer IIx','Illumina HiSeq 2000',
-				'Illumina HiSeq 2500','Illumina HiSeq 4000','Illumina MiSeq','Illumina NextSeq','Ion Torrent PGM',
-				'Ion Torrent Proton','PacBio RS','Other', None]
-		warn, error = process_functions.check_col_and_values(assay_info_df, 'instrument_model', instrument_model, filename="Assay_information.yaml", required=True)
-		warning += warn
-		total_error  += error 
+        instrument_model = ['454 GS FLX Titanium','AB SOLiD 4','AB SOLiD 2','AB SOLiD 3','Complete Genomics','Illumina HiSeq X Ten',
+                'Illumina HiSeq X Five','Illumina Genome Analyzer II','Illumina Genome Analyzer IIx','Illumina HiSeq 2000',
+                'Illumina HiSeq 2500','Illumina HiSeq 4000','Illumina MiSeq','Illumina NextSeq','Ion Torrent PGM',
+                'Ion Torrent Proton','PacBio RS','Other', None]
+        warn, error = process_functions.check_col_and_values(assay_info_df, 'instrument_model', instrument_model, filename="Assay_information.yaml", required=True)
+        warning += warn
+        total_error  += error 
 
-		variant_classes = ['Splice_Site','Nonsense_Mutation','Frame_Shift_Del','Frame_Shift_Ins',
-		'Nonstop_Mutation','Translation_Start_Site','In_Frame_Ins','In_Frame_Del','Missense_Mutation',
-		'Intron','Splice_Region','Silent','RNA',"5'UTR","3'UTR",'IGR',"5'Flank","3'Flank", None]
-		warn, error = process_functions.check_col_and_values(assay_info_df, 'variant_classifications', variant_classes, filename="Assay_information.yaml", na_allowed=True)
-		warning += warn
-		total_error  += error 
+        variant_classes = ['Splice_Site','Nonsense_Mutation','Frame_Shift_Del','Frame_Shift_Ins',
+        'Nonstop_Mutation','Translation_Start_Site','In_Frame_Ins','In_Frame_Del','Missense_Mutation',
+        'Intron','Splice_Region','Silent','RNA',"5'UTR","3'UTR",'IGR',"5'Flank","3'Flank", None]
+        warn, error = process_functions.check_col_and_values(assay_info_df, 'variant_classifications', variant_classes, filename="Assay_information.yaml", na_allowed=True)
+        warning += warn
+        total_error  += error 
 
-		if not process_functions.checkColExist(assay_info_df, "target_capture_kit"):
-			total_error += "Assay_information.yaml: Must have target_capture_kit column.\n"
+        if not process_functions.checkColExist(assay_info_df, "target_capture_kit"):
+            total_error += "Assay_information.yaml: Must have target_capture_kit column.\n"
 
-		if process_functions.checkColExist(assay_info_df, "read_length"):
-			if not all([process_functions.checkInt(i) for i in assay_info_df["read_length"]]):
-				total_error += "Assay_information.yaml: Please double check your read_length.  It must be an integer or 'Unknown'.\n"
-		else:
-			total_error += "Assay_information.yaml: Must have read_length column.\n"
+        if process_functions.checkColExist(assay_info_df, "read_length"):
+            if not all([process_functions.checkInt(i) for i in assay_info_df["read_length"]]):
+                total_error += "Assay_information.yaml: Please double check your read_length.  It must be an integer or 'Unknown'.\n"
+        else:
+            total_error += "Assay_information.yaml: Must have read_length column.\n"
 
-		if process_functions.checkColExist(assay_info_df, "number_of_genes"):
-			if not all([process_functions.checkInt(i) for i in assay_info_df["number_of_genes"]]):
-				total_error += "Assay_information.yaml: Please double check your number_of_genes.  It must be an integer or 'Unknown'.\n"
-		else:
-			total_error += "Assay_information.yaml: Must have number_of_genes column.\n"
-		
-		if process_functions.checkColExist(assay_info_df, "gene_padding"):
-			if not all([process_functions.checkInt(i) for i in assay_info_df["gene_padding"] if i is not None and not pd.isnull(i)]):
-				total_error += "Assay_information.yaml: Please double check your gene_padding.  It must be an integer or blank.\n"
-		else:
-			warning += "Assay_information.yaml: gene_padding is by default 10 if not specified.\n"
+        if process_functions.checkColExist(assay_info_df, "number_of_genes"):
+            if not all([process_functions.checkInt(i) for i in assay_info_df["number_of_genes"]]):
+                total_error += "Assay_information.yaml: Please double check your number_of_genes.  It must be an integer or 'Unknown'.\n"
+        else:
+            total_error += "Assay_information.yaml: Must have number_of_genes column.\n"
+        
+        if process_functions.checkColExist(assay_info_df, "gene_padding"):
+            if not all([process_functions.checkInt(i) for i in assay_info_df["gene_padding"] if i is not None and not pd.isnull(i)]):
+                total_error += "Assay_information.yaml: Please double check your gene_padding.  It must be an integer or blank.\n"
+        else:
+            warning += "Assay_information.yaml: gene_padding is by default 10 if not specified.\n"
 
-		return(total_error, warning)
+        return(total_error, warning)
--- tests/unit/test_assay.py
+++ tests/unit/test_assay.py
@@ -18,66 +18,66 @@ def test_correct_validatefilename():
     assert assay_info.validateFilename(["assay_information.yaml"]) == "assayinfo"
 
 def test_valid__validate():
-	assay_info_dict = {'SEQ_ASSAY_ID':['SAGE-1','SAGE-3'],
-	'is_paired_end':[True,False],
-	'library_strategy':['ChIP-Seq','ChIP-Seq'],
-	'library_selection':['PCR','PCR'],
-	'platform':['Illumina','Illumina'],
-	'instrument_model':['Illumina HiSeq 4000','Illumina HiSeq 4000'],
-	'variant_classifications':['Frame_Shift_Ins','Frame_Shift_Ins'],
-	'target_capture_kit':['foo','doo'],
-	'read_length':[22,333],
-	'number_of_genes':[5,20],
-	'gene_padding':[10, None]}
-	assay_info_df = pd.DataFrame(assay_info_dict)
-	error, warning = assay_info._validate(assay_info_df)
-	assert error == ''
-	assert warning == '' 
+    assay_info_dict = {'SEQ_ASSAY_ID':['SAGE-1','SAGE-3'],
+    'is_paired_end':[True,False],
+    'library_strategy':['ChIP-Seq','ChIP-Seq'],
+    'library_selection':['PCR','PCR'],
+    'platform':['Illumina','Illumina'],
+    'instrument_model':['Illumina HiSeq 4000','Illumina HiSeq 4000'],
+    'variant_classifications':['Frame_Shift_Ins','Frame_Shift_Ins'],
+    'target_capture_kit':['foo','doo'],
+    'read_length':[22,333],
+    'number_of_genes':[5,20],
+    'gene_padding':[10, None]}
+    assay_info_df = pd.DataFrame(assay_info_dict)
+    error, warning = assay_info._validate(assay_info_df)
+    assert error == ''
+    assert warning == '' 
 
 def test_missingcols__validate():
-	assay_info_df = pd.DataFrame()
-	error, warning = assay_info._validate(assay_info_df)
-	expected_errors = ('Assay_information.yaml: Must have SEQ_ASSAY_ID column.\n'
-					   'Assay_information.yaml: Must have is_paired_end column.\n'
-					   'Assay_information.yaml: Must have library_selection column.\n'
-					   'Assay_information.yaml: Must have library_strategy column.\n'
-					   'Assay_information.yaml: Must have platform column.\n'
-					   'Assay_information.yaml: Must have instrument_model column.\n'
-					   'Assay_information.yaml: Must have target_capture_kit column.\n'
-					   'Assay_information.yaml: Must have read_length column.\n'
-					   'Assay_information.yaml: Must have number_of_genes column.\n')
-	assert error == expected_errors
-	expected_warnings = ("Assay_information.yaml: Doesn't have variant_classifications column. This column will be added\n"
-						 "Assay_information.yaml: gene_padding is by default 10 if not specified.\n")
-	assert warning == expected_warnings
+    assay_info_df = pd.DataFrame()
+    error, warning = assay_info._validate(assay_info_df)
+    expected_errors = ('Assay_information.yaml: Must have SEQ_ASSAY_ID column.\n'
+                       'Assay_information.yaml: Must have is_paired_end column.\n'
+                       'Assay_information.yaml: Must have library_selection column.\n'
+                       'Assay_information.yaml: Must have library_strategy column.\n'
+                       'Assay_information.yaml: Must have platform column.\n'
+                       'Assay_information.yaml: Must have instrument_model column.\n'
+                       'Assay_information.yaml: Must have target_capture_kit column.\n'
+                       'Assay_information.yaml: Must have read_length column.\n'
+                       'Assay_information.yaml: Must have number_of_genes column.\n')
+    assert error == expected_errors
+    expected_warnings = ("Assay_information.yaml: Doesn't have variant_classifications column. This column will be added\n"
+                         "Assay_information.yaml: gene_padding is by default 10 if not specified.\n")
+    assert warning == expected_warnings
 
 def test_fillcols__process():
-	'''
-	Standardization of SEQ_ASSAY_ID
-	Add in CENTER, gene_padding, and variant_classifications if missing
-	'''
+    '''
+    Standardization of SEQ_ASSAY_ID
+    Add in CENTER, gene_padding, and variant_classifications if missing
+    '''
 
-	assay_info_dict = {'SEQ_ASSAY_ID':['SAGE-Foo_1']}
-	assay_info_df = pd.DataFrame(assay_info_dict)
-	processed_assay_df = assay_info._process(assay_info_df)
-	expected_assay_df = pd.DataFrame({'SEQ_ASSAY_ID':['SAGE-FOO-1'],
-									  'gene_padding':[10],
-									  'variant_classifications':[float('nan')],
-									  'CENTER':['SAGE']})
-	assert expected_assay_df.equals(processed_assay_df[expected_assay_df.columns])
+    assay_info_dict = {'SEQ_ASSAY_ID':['SAGE-Foo_1']}
+    assay_info_df = pd.DataFrame(assay_info_dict)
+    processed_assay_df = assay_info._process(assay_info_df)
+    expected_assay_df = pd.DataFrame({'SEQ_ASSAY_ID':['SAGE-FOO-1'],
+                                      'gene_padding':[10],
+                                      'variant_classifications':[float('nan')],
+                                      'CENTER':['SAGE']})
+    assert expected_assay_df.equals(processed_assay_df[expected_assay_df.columns])
 
 def test_default10__process():
-	'''
-	gene_padding default 10
-	'''
-	
-	assay_info_dict = {'SEQ_ASSAY_ID':['SAGE-1','SAGE-2'],
-					   'gene_padding':[20,float('nan')],
-					   'variant_classifications':['test','test']}
-	assay_info_df = pd.DataFrame(assay_info_dict)
-	processed_assay_df = assay_info._process(assay_info_df)
-	expected_assay_df = pd.DataFrame({'SEQ_ASSAY_ID':['SAGE-1','SAGE-2'],
-									  'gene_padding':[20, 10],
-									  'variant_classifications':['test','test'],
-									  'CENTER':['SAGE','SAGE']})
-	assert expected_assay_df.equals(processed_assay_df[expected_assay_df.columns])
+    '''
+    gene_padding default 10
+    '''
+    
+    assay_info_dict = {'SEQ_ASSAY_ID':['SAGE-1','SAGE-2'],
+                       'gene_padding':[20,float('nan')],
+                       'variant_classifications':['test','test']}
+    assay_info_df = pd.DataFrame(assay_info_dict)
+    processed_assay_df = assay_info._process(assay_info_df)
+    expected_assay_df = pd.DataFrame({'SEQ_ASSAY_ID':['SAGE-1','SAGE-2'],
+                                      'gene_padding':[20, 10],
+                                      'variant_classifications':['test','test'],
+                                      'CENTER':['SAGE','SAGE']})
+    assert expected_assay_df.equals(processed_assay_df[expected_assay_df.columns])
