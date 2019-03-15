a3da0b87d76802a6241352df2e2334f6db433ce9
thomasyu888 <thomasyu888@gmail.com>
2019-03-14 19:12:28 -0700

    Lint

--- tests/unit/test_process_functions.py
+++ tests/unit/test_process_functions.py
@@ -73,42 +73,47 @@ def test___get_left_union_df():
 
 
 def test_none__get_left_union_df():
-    new_datadf = pd.DataFrame({'UNIQUE_KEY': ['test7', 'test5', 'test4'],
-                          "test": ['test', 'test2', 'test3'],
-                          "foo": [1, 3, 3],
-                          "baz": [float('nan'), 5, float('nan')]})
+    new_datadf = pd.DataFrame({
+        'UNIQUE_KEY': ['test7', 'test5', 'test4'],
+        "test": ['test', 'test2', 'test3'],
+        "foo": [1, 3, 3],
+        "baz": [float('nan'), 5, float('nan')]})
     left_union = genie.process_functions._get_left_union_df(
         new_datadf, DATABASE_DF, 'UNIQUE_KEY')
     assert left_union.empty
 
 
 def test_update__update_rows():
-    new_datadf = pd.DataFrame({'UNIQUE_KEY': ['test1', 'test2', 'test3'],
-                          "test": ['test', 'test2', 'test3'],
-                          "foo": [1, 3, 3],
-                          "baz": [float('nan'), 5, float('nan')]})
+    new_datadf = pd.DataFrame({
+        'UNIQUE_KEY': ['test1', 'test2', 'test3'],
+        "test": ['test', 'test2', 'test3'],
+        "foo": [1, 3, 3],
+        "baz": [float('nan'), 5, float('nan')]})
 
-    expecteddf = pd.DataFrame({"test": ['test', 'test2'],
-                          "foo": [1, 3],
-                          "baz": ['', 5],
-                          'ROW_ID': ['1', '2'],
-                          'ROW_VERSION': ['3', '3']})
+    expecteddf = pd.DataFrame({
+        "test": ['test', 'test2'],
+        "foo": [1, 3],
+        "baz": ['', 5],
+        'ROW_ID': ['1', '2'],
+        'ROW_VERSION': ['3', '3']})
     update_rows = genie.process_functions._update_rows(
         new_datadf, DATABASE_DF, 'UNIQUE_KEY')
     assert update_rows.equals(expecteddf[update_rows.columns])
 
 
 def test_maintaintype__update_rows():
-    new_datadf = pd.DataFrame({'UNIQUE_KEY': ['test1', 'test2', 'test3'],
-                          "test": ['test1', 'test2', 'test3'],
-                          "foo": [1, 3, 3],
-                          "baz": [float('nan'), 5, float('nan')]})
-    #Test that the datatype passed into from new_datadf gets preserved
-    expecteddf = pd.DataFrame({"test": ['test2'],
-                          "foo": [3],
-                          "baz": [5],
-                          'ROW_ID': ['2'],
-                          'ROW_VERSION': ['3']})
+    new_datadf = pd.DataFrame({
+        'UNIQUE_KEY': ['test1', 'test2', 'test3'],
+        "test": ['test1', 'test2', 'test3'],
+        "foo": [1, 3, 3],
+        "baz": [float('nan'), 5, float('nan')]})
+    # Test that the datatype passed into from new_datadf gets preserved
+    expecteddf = pd.DataFrame({
+        "test": ['test2'],
+        "foo": [3],
+        "baz": [5],
+        'ROW_ID': ['2'],
+        'ROW_VERSION': ['3']})
     expecteddf = expecteddf.astype({'baz': object})
     update_rows = genie.process_functions._update_rows(
         new_datadf, DATABASE_DF, 'UNIQUE_KEY')
@@ -116,22 +121,25 @@ def test_maintaintype__update_rows():
 
 
 def test_noupdate__update_rows():
-    new_datadf = pd.DataFrame({'UNIQUE_KEY': ['test4'],
-                                  "test": ['test'],
-                                  "foo": [1],
-                                  "baz": [float('nan')]})
+    new_datadf = pd.DataFrame({
+        'UNIQUE_KEY': ['test4'],
+        "test": ['test'],
+        "foo": [1],
+        "baz": [float('nan')]})
     update_rows = genie.process_functions._update_rows(
         new_datadf, DATABASE_DF, 'UNIQUE_KEY')
     assert update_rows.empty
 
 
 def test_delete__delete_rows():
-    new_datadf = pd.DataFrame({'UNIQUE_KEY': ['test1'],
-                          "test": ['test1'],
-                          "foo": [1],
-                          "baz": [float('nan')]})
-    expecteddf = pd.DataFrame({0: ['2', '3'],
-                               1: ['3', '5']})
+    new_datadf = pd.DataFrame({
+        'UNIQUE_KEY': ['test1'],
+        "test": ['test1'],
+        "foo": [1],
+        "baz": [float('nan')]})
+    expecteddf = pd.DataFrame({
+        0: ['2', '3'],
+        1: ['3', '5']})
     delete_rows = genie.process_functions._delete_rows(
         new_datadf, DATABASE_DF, 'UNIQUE_KEY')
     assert delete_rows.equals(expecteddf)
@@ -141,4 +149,3 @@ def test_norows__delete_rows():
     delete_rows = genie.process_functions._delete_rows(
         DATABASE_DF, DATABASE_DF, 'UNIQUE_KEY')
     assert delete_rows.empty
-
