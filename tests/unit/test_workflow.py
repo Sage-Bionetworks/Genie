56e603d058606249b01e4696b044dacf8a2aac61
Thomas Yu <thomasyu888@gmail.com>
2019-03-20 14:40:17 -0700

    Lint

--- tests/unit/test_workflow.py
+++ tests/unit/test_workflow.py
@@ -1,24 +1,18 @@
 import synapseclient
-import pandas as pd
 import mock
-from nose.tools import assert_raises
-import os
-import sys
+import pytest
 from genie.workflow import workflow
 
+syn = mock.create_autospec(synapseclient.Synapse)
+workflowClass = workflow(syn, "SAGE")
+
+
 def test_processing():
-
-    syn = mock.create_autospec(synapseclient.Synapse) 
-
-    workflowClass = workflow(syn, "SAGE")
     pass
 
+
 def test_validation():
-
-    syn = mock.create_autospec(synapseclient.Synapse) 
-
-    workflowClass = workflow(syn, "SAGE")
-
-    assert_raises(AssertionError, workflowClass.validateFilename, ["foo"])
-    assert_raises(AssertionError, workflowClass.validateFilename, ["SAGE-test.txt"])
+    with pytest.raises(AssertionError):
+        workflowClass.validateFilename(["foo"])
+        workflowClass.validateFilename(["SAGE-test.txt"])
     assert workflowClass.validateFilename(["SAGE-test.md"]) == "md"
