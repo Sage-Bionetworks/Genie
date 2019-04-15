6031fc6830a3f328f74dc53f81368fd9d2f221b6
Thomas Yu <thomasyu888@gmail.com>
2019-04-15 15:34:31 -0700

    Deprecate but clean up

--- genie/patientCounts.py
+++ genie/patientCounts.py
@@ -7,7 +7,9 @@ logger = logging.getLogger(__name__)
 
 
 class patientCounts(FileTypeFormat):
-
+    '''
+    Deprecated class
+    '''
     _fileType = "patientCounts"
 
     _process_kwargs = ["newPath", "oncotreeLink", "databaseSynId"]
@@ -32,7 +34,7 @@ class patientCounts(FileTypeFormat):
         patientCountsDf = self._process(patientCountsDf, oncotreeLink)
         process_functions.updateData(
             self.syn, databaseSynId, patientCountsDf, self.center)
-        patientCountsDf.to_csv(newPath, sep="\t",index=False)
+        patientCountsDf.to_csv(newPath, sep="\t", index=False)
         return(newPath)
 
     def _validate(self, patCountsDf, oncotreeLink):
@@ -44,21 +46,39 @@ class patientCounts(FileTypeFormat):
         oncotree_mapping_dict = \
             process_functions.get_oncotree_code_mappings(oncotreeLink)
         oncotree_mapping['ONCOTREE_CODE'] = oncotree_mapping_dict.keys()
-        haveColumn = process_functions.checkColExist(patCountsDf, "ONCOTREE_CODE")
+        haveColumn = \
+            process_functions.checkColExist(patCountsDf, "ONCOTREE_CODE")
         if haveColumn:
             if sum(patCountsDf['ONCOTREE_CODE'].duplicated()) > 0:
-                total_error += "Patient Counts: Must not have any duplicated ONCOTREE CODES.\n"
-            if not all(patCountsDf['ONCOTREE_CODE'].isin(oncotree_mapping['ONCOTREE_CODE'])):
-                unmapped_oncotrees = patCountsDf['ONCOTREE_CODE'][~patCountsDf['ONCOTREE_CODE'].isin(oncotree_mapping['ONCOTREE_CODE'])]
-                total_error += "Patient Counts: Please double check that all your ONCOTREE CODES exist in the mapping. You have %d codes that don't map. These are the codes that don't map: %s\n" % (len(unmapped_oncotrees),",".join(set(unmapped_oncotrees)))
+                total_error += (
+                    "Patient Counts: "
+                    "Must not have any duplicated ONCOTREE CODES.\n")
+            if not all(patCountsDf['ONCOTREE_CODE'].isin(
+                    oncotree_mapping['ONCOTREE_CODE'])):
+                unmapped_oncotrees = patCountsDf['ONCOTREE_CODE'][
+                    ~patCountsDf['ONCOTREE_CODE'].isin(
+                        oncotree_mapping['ONCOTREE_CODE'])]
+                total_error += (
+                    "Patient Counts: Please double check that all your "
+                    "ONCOTREE CODES exist in the mapping. You have {} codes "
+                    "that don't map. These are the codes that "
+                    "don't map: {}\n".format(
+                        len(unmapped_oncotrees),
+                        ",".join(set(unmapped_oncotrees))))
         else:
-            total_error += "Patient Counts: File must have ONCOTREE_CODE column.\n"
+            total_error += (
+                "Patient Counts: File must have ONCOTREE_CODE column.\n")
 
-        haveColumn = process_functions.checkColExist(patCountsDf, "NUM_PATIENTS_PD1_PDL1")
+        haveColumn = process_functions.checkColExist(
+            patCountsDf, "NUM_PATIENTS_PD1_PDL1")
         if haveColumn:
-            if not all([isinstance(i, int) for i in patCountsDf['NUM_PATIENTS_PD1_PDL1']]):
-                total_error += "Patient Counts: Must not have any null values, and must be all integers.\n"
+            if not all([isinstance(i, int)
+                        for i in patCountsDf['NUM_PATIENTS_PD1_PDL1']]):
+                total_error += (
+                    "Patient Counts: Must not have any null values, "
+                    "and must be all integers.\n")
         else:
-            total_error += "Patient Counts: File must have NUM_PATIENTS_PD1_PDL1 column.\n"
-
+            total_error += (
+                "Patient Counts: File must have "
+                "NUM_PATIENTS_PD1_PDL1 column.\n")
         return(total_error, warning)
