e1b5b59b9d542b70d05b8153a9a0504d0b907911
Thomas Yu <thomasyu888@gmail.com>
2019-04-15 15:29:26 -0700

    Deprecated but must have working unit tests

--- tests/unit/deprecated_test_patientCounts.py
+++ tests/unit/deprecated_test_patientCounts.py
@@ -4,77 +4,103 @@ import mock
 import pytest
 from genie import patientCounts
 
-oncotree_url = 'http://oncotree.mskcc.org/api/tumor_types.txt?version=oncotree_latest_stable'
-json_oncotreeurl = "http://oncotree.mskcc.org/api/tumorTypes/tree?version=oncotree_2017_06_21"
+json_oncotreeurl = \
+    "http://oncotree.mskcc.org/api/tumorTypes/tree?version=oncotree_2017_06_21"
 
 syn = mock.create_autospec(synapseclient.Synapse)
 
 pc = patientCounts(syn, "SAGE")
 
+onco_map_dict = {
+  "AMPCA": {
+    'CANCER_TYPE': "Ampullary Cancer",
+    'CANCER_TYPE_DETAILED': "Ampullary Carcinoma",
+    'ONCOTREE_PRIMARY_NODE': "AMPULLA_OF_VATER",
+    'ONCOTREE_SECONDARY_NODE': "AMPCA"},
+  "TESTIS": {
+    'CANCER_TYPE': "Testicular Cancer, NOS",
+    'CANCER_TYPE_DETAILED': "Testis",
+    'ONCOTREE_PRIMARY_NODE': "TESTIS",
+    'ONCOTREE_SECONDARY_NODE': ''},
+  "UCEC": {
+    'CANCER_TYPE': "Endometrial Cancer",
+    'CANCER_TYPE_DETAILED': "Endometrial Carcinoma",
+    'ONCOTREE_PRIMARY_NODE': "UTERUS",
+    'ONCOTREE_SECONDARY_NODE': "UCEC"}}
+
 
 def test_processing():
-
     expectedpcDf = pd.DataFrame(dict(
-        CENTER=['SAGE', 'SAGE', 'SAGE', 'SAGE', 'SAGE'],
-        ONCOTREE_CODE=['LUAD', 'AMPCA', 'CHOL', 'IHCH', 'UCA'],
-        NUM_PATIENTS_PD1_PDL1=[1, 2, 3, 4, 3],
-        PRIMARY_CODE=['LUNG', 'AMPULLA_OF_VATER', 'BILIARY_TRACT',
-                      'BILIARY_TRACT', 'BLADDER']))
+        CENTER=['SAGE', 'SAGE', 'SAGE'],
+        ONCOTREE_CODE=['AMPCA', 'UCEC', 'TESTIS'],
+        NUM_PATIENTS_PD1_PDL1=[1, 2, 3],
+        PRIMARY_CODE=['AMPULLA_OF_VATER', 'UTERUS', 'TESTIS']))
 
     pcDf = pd.DataFrame(dict(
-        CENTER=['foo', 'foo', 'foo', 'foo', 'foo'],
-        ONCOTREE_CODE=['LUAD', 'AMPCA', 'CHOL', 'IHCH', 'UCA'],
-        NUM_PATIENTS_PD1_PDL1=[1, 2, 3, 4, 3]))
+        CENTER=['foo', 'foo', 'foo'],
+        ONCOTREE_CODE=['AMPCA', 'UCEC', 'TESTIS'],
+        NUM_PATIENTS_PD1_PDL1=[1, 2, 3]))
+    with mock.patch(
+            "genie.process_functions.get_oncotree_code_mappings",
+            return_value=onco_map_dict) as mock_get_onco_map:
+        newpcDf = pc._process(pcDf, json_oncotreeurl)
+        mock_get_onco_map.called_once_with(json_oncotreeurl)
+        assert expectedpcDf.equals(newpcDf[expectedpcDf.columns])
 
-    newpcDf = pc._process(pcDf, oncotree_url)
-    assert expectedpcDf.equals(newpcDf[expectedpcDf.columns])
 
-
-def test_validation():
-    with pytest.raises(AssertionError):
-        pc.validateFilename(["foo"])
+def test_perfect_validateFilename():
     assert pc.validateFilename(["patient_counts.txt"]) == "patientCounts"
 
-    pcDf = pd.DataFrame(dict(
-        CENTER=['foo', 'foo', 'foo', 'foo', 'foo'],
-        ONCOTREE_CODE=['LUAD', 'AMPCA', 'CHOL', 'IHCH', 'UCA'],
-        NUM_PATIENTS_PD1_PDL1=[1, 2, 3, 4, 3]))
 
-    error, warning = pc._validate(pcDf, oncotree_url)
-    assert error == ""
-    assert warning == ""
-    error, warning = pc._validate(pcDf, json_oncotreeurl)
-    assert error == ""
-    assert warning == ""
+def test_invalid_validateFilename():
+    with pytest.raises(AssertionError):
+        pc.validateFilename(["foo"])
 
+
+def test_perfect__validate():
     pcDf = pd.DataFrame(dict(
-        ONCOTREE_CODE=['LUAD', 'LUAD', 'CHOLD', 'IHCH', 'UCA'],
+        CENTER=['foo', 'foo', 'foo'],
+        ONCOTREE_CODE=['AMPCA', 'UCEC', 'TESTIS'],
+        NUM_PATIENTS_PD1_PDL1=[1, 2, 3]))
+    with mock.patch(
+            "genie.process_functions.get_oncotree_code_mappings",
+            return_value=onco_map_dict) as mock_get_onco_map:
+        error, warning = pc._validate(pcDf, json_oncotreeurl)
+        mock_get_onco_map.called_once_with(json_oncotreeurl)
+        assert error == ""
+        assert warning == ""
+
+
+def test_invalid__validate():
+    pcDf = pd.DataFrame(dict(
+        ONCOTREE_CODE=['AMPCA', 'AMPCA', 'CHOLD', 'AMPCA', 'TESTIS'],
         NUM_PATIENTS_PD1_PDL1=[1, 2, 3, 4, float('nan')]))
+    with mock.patch(
+            "genie.process_functions.get_oncotree_code_mappings",
+            return_value=onco_map_dict):
+        error, warning = pc._validate(pcDf, json_oncotreeurl)
+        expectedErrors = (
+            "Patient Counts: Must not have any duplicated ONCOTREE CODES.\n"
+            "Patient Counts: Please double check that all your ONCOTREE CODES "
+            "exist in the mapping. You have 1 codes that don't map. These are "
+            "the codes that don't map: CHOLD\n"
+            "Patient Counts: Must not have any null values, "
+            "and must be all integers.\n")
+        assert error == expectedErrors
+        assert warning == ""
 
-    error, warning = pc._validate(pcDf, json_oncotreeurl)
-    expectedErrors = (
-        "Patient Counts: Must not have any duplicated ONCOTREE CODES.\n"
-        "Patient Counts: Please double check that all your ONCOTREE CODES "
-        "exist in the mapping. You have 1 codes that don't map. These are "
-        "the codes that don't map: CHOLD\n"
-        "Patient Counts: Must not have any null values, "
-        "and must be all integers.\n")
-    assert error == expectedErrors
-    assert warning == ""
 
-    pcDf = pd.DataFrame(dict(
-        ONCOTREE_CODE=['LUAD', 'AMPCA', 'CHOL', 'IHCH', 'UCA']))
-    error, warning = pc._validate(pcDf, json_oncotreeurl)
-
-    expectedErrors = (
-        "Patient Counts: File must have NUM_PATIENTS_PD1_PDL1 column.\n")
-
-    assert error == expectedErrors
-    assert warning == ""
-
-    pcDf = pd.DataFrame(dict(NUM_PATIENTS_PD1_PDL1=[1, 2, 3, 4, 5]))
-    error, warning = pc._validate(pcDf, json_oncotreeurl)
-
-    expectedErrors = ("Patient Counts: File must have ONCOTREE_CODE column.\n")
-    assert error == expectedErrors
-    assert warning == ""
+def test_missingcol__validate():
+    '''
+    Test for missing columns
+    '''
+    pcdf = pd.DataFrame()
+    with mock.patch(
+            "genie.process_functions.get_oncotree_code_mappings",
+            return_value=onco_map_dict):
+        error, warning = pc._validate(pcdf, json_oncotreeurl)
+        expectedErrors = (
+            "Patient Counts: File must have ONCOTREE_CODE column.\n"
+            "Patient Counts: File must have NUM_PATIENTS_PD1_PDL1 column.\n")
+        assert error == expectedErrors
+        assert warning == ""
