56471049d91aac7cf8d2371d29c5c7e71321e097
thomasyu888 <thomasyu888@gmail.com>
2019-05-09 14:46:58 -0700

    dict auto sorts in < python3.6

--- tests/unit/test_create_case_lists.py
+++ tests/unit/test_create_case_lists.py
@@ -11,16 +11,16 @@ clinical_file_map = {
 
 case_list_files = create_case_lists.write_case_list_files(
     clinical_file_map, "./", study_id)
-#sequenced_case_list_files = create_case_lists.write_case_list_sequenced(
-#    ['test1', 'test2'], "./", study_id)
+# sequenced_case_list_files = create_case_lists.write_case_list_sequenced(
+#     ['test1', 'test2'], "./", study_id)
 
 
 def test_filenames_write_case_list_files():
-    first = os.path.basename(case_list_files[0])
+    first = os.path.basename(case_list_files[2])
     assert first == "cases_Test1_Now_Please_foo.txt"
-    second = os.path.basename(case_list_files[1])
+    second = os.path.basename(case_list_files[0])
     assert second == "cases_no_oncotree_code.txt"
-    third = os.path.basename(case_list_files[2])
+    third = os.path.basename(case_list_files[1])
     assert third == "cases_UNKNOWN.txt"
 
 
@@ -32,10 +32,10 @@ def test_textfix_write_case_list_files():
         'case_list_description: All tumors with cancer type '
         'Test1 Now, Please/foo\n'
         'case_list_ids: wow')
-    with open(case_list_files[0], 'r') as case_list:
+    with open(case_list_files[2], 'r') as case_list:
         caselist_text = case_list.read()
     assert caselist_text == expected_text
-    os.remove(case_list_files[0])
+    os.remove(case_list_files[2])
 
 
 def test_nocode_write_case_list_files():
@@ -45,10 +45,10 @@ def test_nocode_write_case_list_files():
         'case_list_name: Tumor Type: NA\n'
         'case_list_description: All tumors with cancer type NA\n'
         'case_list_ids: FOOBAR\tNEED')
-    with open(case_list_files[1], 'r') as case_list:
+    with open(case_list_files[0], 'r') as case_list:
         caselist_text = case_list.read()
     assert caselist_text == expected_text
-    os.remove(case_list_files[1])
+    os.remove(case_list_files[0])
 
 
 def test_nochange_write_case_list_files():
@@ -58,7 +58,7 @@ def test_nochange_write_case_list_files():
         'case_list_name: Tumor Type: UNKNOWN\n'
         'case_list_description: All tumors with cancer type UNKNOWN\n'
         'case_list_ids: test1')
-    with open(case_list_files[2], 'r') as case_list:
+    with open(case_list_files[1], 'r') as case_list:
         caselist_text = case_list.read()
     assert caselist_text == expected_text
-    os.remove(case_list_files[2])
+    os.remove(case_list_files[1])
