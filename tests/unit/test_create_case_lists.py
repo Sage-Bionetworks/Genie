77487d982347865cffe346aa6084fc506c735772
thomasyu888 <thomasyu888@gmail.com>
2019-05-12 21:13:01 -0700

    Use Testing2

--- tests/unit/test_create_case_lists.py
+++ tests/unit/test_create_case_lists.py
@@ -4,10 +4,10 @@ import os
 
 study_id = "test"
 
-clinical_file_map = {
-    '': ['FOOBAR', 'NEED'],
-    'UNKNOWN': ['test1'],
-    "Test1 Now, Please/foo": ["wow"]}
+clinical_file_map = {'': ['FOOBAR', 'NEED']}
+clinical_file_map['Testing2'] = ['test1']
+clinical_file_map['Test1 Now, Please/foo'] = ['wow']
+
 
 case_list_files = create_case_lists.write_case_list_files(
     clinical_file_map, "./", study_id)
@@ -25,7 +25,7 @@ def test_filenames_write_case_list_files():
     second = os.path.basename(case_list_files[0])
     assert second == "cases_no_oncotree_code.txt"
     third = os.path.basename(case_list_files[1])
-    assert third == "cases_UNKNOWN.txt"
+    assert third == "cases_Testing2.txt"
 
 
 def test_textfix_write_case_list_files():
@@ -58,9 +58,9 @@ def test_nocode_write_case_list_files():
 def test_nochange_write_case_list_files():
     expected_text = (
         'cancer_study_identifier: test\n'
-        'stable_id: test_UNKNOWN\n'
-        'case_list_name: Tumor Type: UNKNOWN\n'
-        'case_list_description: All tumors with cancer type UNKNOWN\n'
+        'stable_id: test_Testing2\n'
+        'case_list_name: Tumor Type: Testing2\n'
+        'case_list_description: All tumors with cancer type Testing2\n'
         'case_list_ids: test1')
     with open(case_list_files[1], 'r') as case_list:
         caselist_text = case_list.read()
