de701c407a25188524c142ca09fb395f458252c9
kdaily <kenneth.daily@sagebase.org>
2019-05-21 11:17:50 -0700

    separate input to db main into a script

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='aacrgenie',
       zip_safe=False,
       entry_points={
         'console_scripts': ['genie = genie.__main__:main']},
+      scripts=['bin/input_to_database.py'],
       install_requires=[
         'pandas>=0.20.0',
         'synapseclient>=1.9',
