e39a122dcfc9a71534d94f5e867406fd81d38f4b
kdaily <kenneth.daily@sagebase.org>
2019-05-21 16:52:27 -0700

    remove unused fxns

--- genie/veoibd_clinical.py
+++ genie/veoibd_clinical.py
@@ -13,57 +13,6 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def checkMapping(
-        clinicalDF, colName, mapping, required=False, fileType="Patient"):
-    """
-    This function checks if the column exists then checks if the
-    values in the column have the correct integer values
-
-    :params clinicalDF          Patient/sample/flattened clinical file
-    :params colName:            Expected column name
-    :params mapping:            List of possible values
-
-    :returns:                   A tuple warning, error
-    """
-    warning = ""
-    error = ""
-    haveColumn = process_functions.checkColExist(clinicalDF, colName)
-    if not haveColumn:
-        if required:
-            error = "{}: clinical file must have {} column.\n".format(
-                fileType, colName)
-        else:
-            warning = (
-                "{}: clinical file doesn't have {} column. "
-                "A blank column will be added\n".format(fileType, colName))
-    else:
-        if not all([i in mapping.tolist() for i in clinicalDF[colName]]):
-            error = (
-                "{}: Please double check your {} column.  "
-                "This column must be these values {}or blank.\n".format(
-                    fileType,
-                    colName,
-                    ", ".join(map(str, mapping)).replace(".0", "")))
-    return(warning, error)
-
-
-def remove_greaterthan_lessthan_str(col):
-    '''
-    In clinical file, there are redacted value such as >89 and <17.
-    These < and > signs must be removed
-    '''
-    try:
-        col = [
-            text.replace(">", "")
-            if isinstance(text, str) else text for text in col]
-        col = [
-            int(text.replace("<", ""))
-            if isinstance(text, str) and text != "" else text
-            for text in col]
-    except ValueError:
-        pass
-    return(col)
-
 
 class clinical_individual(FileTypeFormat):
 
