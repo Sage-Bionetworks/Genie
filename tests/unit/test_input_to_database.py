3e8eb35a098e105e64010fbb054489b4d6ba50c5
Thomas Yu <thomasyu888@gmail.com>
2019-05-23 11:52:15 -0700

    Remove get filetype tests

--- tests/unit/test_input_to_database.py
+++ tests/unit/test_input_to_database.py
@@ -147,24 +147,24 @@ def test_empty_get_center_input_files():
     os.remove(filename)
 
 
-@pytest.fixture(params=[
-    # tuple with (input, expectedOutput)
-    (["data_CNA_SAGE.txt"], "cna"),
-    (["data_clinical_supp_SAGE.txt"], "clinical"),
-    (["data_clinical_supp_sample_SAGE.txt",
-      "data_clinical_supp_patient_SAGE.txt"], "clinical")])
-def filename_fileformat_map(request):
-    return request.param
+# @pytest.fixture(params=[
+#     # tuple with (input, expectedOutput)
+#     (["data_CNA_SAGE.txt"], "cna"),
+#     (["data_clinical_supp_SAGE.txt"], "clinical"),
+#     (["data_clinical_supp_sample_SAGE.txt",
+#       "data_clinical_supp_patient_SAGE.txt"], "clinical")])
+# def filename_fileformat_map(request):
+#     return request.param
 
 
-def test_perfect_get_filetype(filename_fileformat_map):
-    (filepath_list, fileformat) = filename_fileformat_map
-    assert input_to_database.get_filetype(
-        syn, filepath_list, center) == fileformat
+# def test_perfect_get_filetype(filename_fileformat_map):
+#     (filepath_list, fileformat) = filename_fileformat_map
+#     assert input_to_database.get_filetype(
+#         syn, filepath_list, center) == fileformat
 
 
-def test_wrongfilename_get_filetype():
-    assert input_to_database.get_filetype(syn, ['wrong.txt'], center) is None
+# def test_wrongfilename_get_filetype():
+#     assert input_to_database.get_filetype(syn, ['wrong.txt'], center) is None
 
 
 def test_unvalidatedinput_check_existing_file_status():
