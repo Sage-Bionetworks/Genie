f5118bb50825e3b93de123c1eaf105e956991f6e
Thomas Yu <thomasyu888@gmail.com>
2019-05-23 12:04:39 -0700

    Add unit tests for determining validity and log

--- tests/unit/test_validate.py
+++ tests/unit/test_validate.py
@@ -17,13 +17,20 @@ def filename_fileformat_map(request):
     return request.param
 
 
-def test_perfect_get_filetype(filename_fileformat_map):
+def test_perfect_determine_filetype(filename_fileformat_map):
+    '''
+    Tests determining of file type through filenames
+    Parameters are passed in from filename_fileformat_map
+    '''
     (filepath_list, fileformat) = filename_fileformat_map
     assert validate.determine_filetype(
         syn, filepath_list, center) == fileformat
 
 
-def test_wrongfilename_get_filetype():
+def test_wrongfilename_determine_filetype():
+    '''
+    Tests ValueError is raised when wrong filename is passed
+    '''
     with pytest.raises(
         ValueError,
         match="Your filename is incorrect! "
@@ -31,3 +38,43 @@ def test_wrongfilename_get_filetype():
               "the validator or specify --filetype if you are "
               "running the validator locally"):
         validate.determine_filetype(syn, ['wrong.txt'], center)
+
+
+def test_valid_determine_validity_and_log():
+    '''
+    Tests if no error and warning strings are passed that
+    returned valid and message is correct
+    '''
+    valid, message = \
+        validate.determine_validity_and_log('', '')
+    assert valid
+    assert message == "YOUR FILE IS VALIDATED!\n"
+
+
+def test_invalid_determine_validity_and_log():
+    '''
+    Tests if error and warnings strings are passed that
+    returned valid and message is correct
+    '''
+    valid, message = \
+        validate.determine_validity_and_log("error\nnow", 'warning\nnow')
+    assert not valid
+    assert message == (
+        "----------------ERRORS----------------\n"
+        "error\nnow"
+        "-------------WARNINGS-------------\n"
+        'warning\nnow')
+
+
+def test_warning_determine_validity_and_log():
+    '''
+    Tests if no error but warnings strings are passed that
+    returned valid and message is correct
+    '''
+    valid, message = \
+        validate.determine_validity_and_log('', 'warning\nnow')
+    assert valid
+    assert message == (
+        "YOUR FILE IS VALIDATED!\n"
+        "-------------WARNINGS-------------\n"
+        'warning\nnow')
