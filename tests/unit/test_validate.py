1d3051628d22b722dd9c6e9ff7ccb813a44f749e
kdaily <kenneth.daily@sagebase.org>
2019-09-11 14:39:13 -0700

    fix expected parameters

--- tests/unit/test_validate.py
+++ tests/unit/test_validate.py
@@ -103,7 +103,7 @@ def test_valid_validate_single_file():
 
         validator = validate.GenieValidationHelper(syn, center=center, filepathlist=filepathlist)
 
-        valid, message, filetype = validator.validate_single_file()
+        valid, message, filetype = validator.validate_single_file(oncotreeLink=None, noSymbolCheck=False)
 
         assert valid == expected_valid
         assert message == expected_message
@@ -114,7 +114,6 @@ def test_valid_validate_single_file():
         mock_genie_class.assert_called_once_with(
             filePathList=filepathlist,
             oncotreeLink=None,
-            testing=False,
             noSymbolCheck=False)
 
         mock_determine.assert_called_once_with(error_string, warning_string)
@@ -130,7 +129,7 @@ def test_filetype_validate_single_file():
     expected_error = "----------------ERRORS----------------\nYour filename is incorrect! Please change your filename before you run the validator or specify --filetype if you are running the validator locally"
     validator = validate.GenieValidationHelper(syn, center, filepathlist)
 
-    valid, message, filetype = validator.validate_single_file(filetype="foobar")
+    valid, message, filetype = validator.validate_single_file()
     assert message == expected_error
 
 
@@ -294,8 +293,7 @@ def test_perform_validate():
         patch_syn_tablequery.assert_called_once_with('select * from syn123')
         patch_check_center.assert_called_once_with(arg.center, ["try", "foo"])
         patch_get_onco.assert_called_once()
-        patch_validate.assert_called_once_with(arg.filetype,
-                                               arg.oncotreelink, arg.testing,
-                                               arg.nosymbol_check)
+        patch_validate.assert_called_once_with(oncotreeLink=arg.oncotreelink,
+                                               noSymbolCheck=arg.nosymbol_check)
         patch_syn_upload.assert_called_once_with(
             syn, arg.filepath, valid, parentid=arg.parentid)
