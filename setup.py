72cba65ba01fb19f19623e743b430695a649d564
Thomas Yu <thomasyu888@gmail.com>
2019-09-12 12:49:41 -0700

    Bump version to 3.2

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
+"""genie package setup"""
 from setuptools import setup, find_packages
 
 setup(name='aacrgenie',
-      version='1.6.2',
+      version='3.2',
       description='Processing and validation for GENIE',
       url='https://github.com/Sage-Bionetworks/Genie',
       author='Thomas Yu',
-      author_email='thomasyu888@gmail.com',
+      author_email='thomas.yu@sagebionetworks.org',
       license='MIT',
       packages=find_packages(),
       zip_safe=False,
-      entry_points={
-        'console_scripts': ['genie = genie.__main__:main']},
-      install_requires=[
-        'pandas>=0.20.0',
-        'synapseclient>=1.9',
-        'httplib2>=0.11.3',
-        'pycrypto>=2.6.1',
-        'PyYAML>=3.11'])
+      entry_points={'console_scripts': ['genie = genie.__main__:main']},
+      install_requires=['pandas>=0.20.0',
+                        'synapseclient>=1.9',
+                        'httplib2>=0.11.3',
+                        'pycrypto>=2.6.1',
+                        'PyYAML>=3.11'])
