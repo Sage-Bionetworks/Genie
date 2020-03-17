9d974c48f7b8b71dcc0af710d35ac09541b01902
thomasyu888 <thomasyu888@gmail.com>
2020-03-17 13:59:12 -0700

    Fix dashboard template path

--- bin/database_to_staging.py
+++ bin/database_to_staging.py
@@ -221,7 +221,8 @@ def main(genie_version,
                          '../genie/dashboard_markdown_generator.R'),
             genie_version,
             '--template_path',
-            '../genie/dashboardTemplate.Rmd']
+            os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         '../genie/dashboardTemplate.Rmd')]
 
         if genie_user is not None and genie_pass is not None:
             dashboard_markdown_html_commands.extend(
