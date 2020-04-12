0fafd6781ff198424746a4a166237959bc16975d
Thomas Yu <thomasyu888@gmail.com>
2020-04-12 00:08:03 -0700

    Push long description

--- setup.py
+++ setup.py
@@ -8,9 +8,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "genie", "__version__.py")) as f:
     exec(f.read(), about)
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='synapsegenie',
       version=about["__version__"],
       description='Processing and validation for GENIE',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/Sage-Bionetworks/Genie',
       author='Thomas Yu',
       author_email='thomas.yu@sagebionetworks.org',
