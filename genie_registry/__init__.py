638c86654ed83e592922c85b7885774412b72998
Thomas Yu <thomasyu888@gmail.com>
2020-05-30 15:39:12 -0700

    Add registry

---
+++ genie_registry/__init__.py
@@ -0,0 +1,25 @@
+"""Initialize GENIE registry"""
+# Import logging last to not take in synapseclient logging
+import logging
+
+from . import bed
+from . import vcf
+from . import bedSP
+from . import workflow
+from . import clinical
+from . import seg
+from . import cbs
+from . import maf
+from . import mafSP
+from . import clinicalSP
+from . import cna
+from . import fusions
+from . import sampleRetraction
+from . import patientRetraction
+from . import mutationsInCis
+from . import assay
+
+from genie.__version__ import __version__
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
