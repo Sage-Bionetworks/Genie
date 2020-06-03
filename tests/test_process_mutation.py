97e29dda110ce9978676634641692ebca1a26482
Thomas Yu <thomasyu888@gmail.com>
2020-06-02 17:25:27 -0700

    Fix tests

--- tests/test_process_mutation.py
+++ tests/test_process_mutation.py
@@ -1,4 +1,5 @@
 """Test process mutation functions"""
+import shutil
 from unittest.mock import patch
 
 import pandas as pd
@@ -47,6 +48,8 @@ class TestDtype():
                                     "bar": ["baz"]})
         self.column_types = {"foo": 'int64',
                              "bar": 'object'}
+        self.mutation_path = "/path/test.csv"
+        self.input_dir = "/my/dir/here"
 
     def test__rename_column_headers(self):
         """Tests the renaming of column headers"""
@@ -56,13 +59,50 @@ class TestDtype():
         assert all(newdf.columns == ["new_foo", "new_bar"])
 
     def test_determine_dtype(self):
+        """Tests determining dtype"""
         with patch.object(pd, "read_csv", return_value=self.testdf):
             col_types = process_mutation.determine_dtype("test.csv")
             assert col_types == self.column_types
 
     def test__convert_to_str_dtype(self):
+        """Tests converting dtypes to str dtypes"""
         new_column_types = process_mutation._convert_to_str_dtype(
             self.column_types, ["foo"]
         )
         assert new_column_types == {"foo": 'object',
                                     "bar": 'object'}
+
+    def test_move_maf_copy(self):
+        """Test moving mafs that don't need to rename columns"""
+        with patch.object(pd, "read_csv",
+                          return_value=self.testdf) as patch_read,\
+             patch.object(shutil, "copy") as patch_copy:
+            process_mutation.move_maf(self.mutation_path, self.input_dir)
+            patch_read.assert_called_once_with(
+                self.mutation_path, sep="\t", index_col=0, nrows=1,
+                comment="#"
+            )
+            patch_copy.assert_called_once_with(self.mutation_path,
+                                               self.input_dir)
+
+    def test_move_maf_rename(self):
+        """Test moving mafs when maf column headers need to be remapped"""
+        testdf = pd.DataFrame({"CHROMOSOME": [1]})
+        with patch.object(pd, "read_csv", return_value=testdf),\
+            patch.object(process_mutation,
+                         "determine_dtype",
+                         return_value=self.column_types) as patch_determine,\
+            patch.object(process_mutation,
+                         "_convert_to_str_dtype",
+                         return_value=self.column_types) as patch_convert,\
+            patch.object(process_mutation,
+                         "_rename_column_headers") as patch_rename,\
+            patch.object(testdf, "to_csv"):
+            process_mutation.move_maf(self.mutation_path, self.input_dir)
+            patch_determine.assert_called_once_with(self.mutation_path)
+            patch_convert.assert_called_once_with(
+                self.column_types, process_mutation.KNOWN_STRING_COLS
+            )
+            patch_rename.assert_called_once_with(
+                testdf, col_map=process_mutation.MAF_COL_MAPPING
+            )
