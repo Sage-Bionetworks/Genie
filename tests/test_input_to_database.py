12235d3cda6a114b3f8c8ce4c5629a4589554d71
Thomas Yu <thomasyu888@gmail.com>
2020-06-10 14:50:36 -0700

    Import Mock

--- tests/test_input_to_database.py
+++ tests/test_input_to_database.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import os
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 import pandas as pd
 import pytest
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,6 +1,6 @@
 """Tests validate.py"""
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 import pandas as pd
 import pytest
