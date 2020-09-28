412f6abdcdcf321c67e0a2c9a6439a3d731db31c
Thomas Yu <thomas.yu@sagebase.org>
2020-09-28 16:27:13 -0700

    Notification of retraction - GENIE policy (#352)
    * Add in notification of retraction
    
    * Add in comments
    
    * Add retraction notification to dashboard
    
    * Add line breaks
    
    * Fix retraction policy text
    
    * Removed python notify retraction script
    
    * Fix rmarkdown generator
    
    * Shift retraction policy up in front
    
    * Add number of new samples per release compared to the latest public release (#359)
    
    * Add SEQ_YEAR distribution and number of samples added since last public release
    
    * Fix
    
    * Move new samples added to top
    
    * Comment out SEQ_YEAR dist for now
    
    * update MSK code
    
    * Comment

--- genie/dashboard_markdown_generator.R
+++ genie/dashboard_markdown_generator.R
@@ -25,47 +25,11 @@ template_path <- args$template_path
 suppressPackageStartupMessages(library(synapser))
 suppressPackageStartupMessages(library(rmarkdown))
 
-create_markdown <- function(release, release_folder_synid, template) {
-  #This function creates the markdown file for the release dashboard
-  #params:
-  # release:  Release number
-  # release_folder_synid:  Synapse id of the release
-  # template:  the dashboard template found in this directory read. 
-
-  final_text = ""
-  count = 0
-  for (line in template) {
-    if (final_text != "") {
-      fill_in_text = release
-      #This count is so that the synId gets replaced at the right sprintf
-      if (count == 1) {
-        fill_in_text = release_folder_synid
-      }
-      if (grepl("%s",line)) {
-        final_text = paste(final_text, 
-                           sprintf(line, fill_in_text),
-                           sep = "\n")
-        count = count + 1
-      } else{
-        final_text = paste(final_text, line, sep = "\n")
-      }
-    } else {
-      #This is so that the start of the Rmd isn't an empty line
-      final_text = line
-    }
-  }
-  #The file that gets written out is the release.Rmd
-  write(final_text, sprintf("%s.Rmd", release))
-  return(sprintf("%s.Rmd", release))
-}
-
-
 tryCatch({
   synLogin()
 }, error = function(err) {
   synLogin(genie_user, genie_pass)
 })
-#options(bitmapType='cairo')
 
 if (args$staging) {
  database_synid_mappingid = 'syn12094210'
@@ -75,24 +39,25 @@ if (args$staging) {
  database_synid_mappingid = 'syn10967259'
 }
 
-#Should use this to create a staging version of the dashboard
-database_synid_mapping = synTableQuery(sprintf('select * from %s', database_synid_mappingid))
-database_synid_mappingdf = as.data.frame(database_synid_mapping)
-release_folder_fileview_synid = database_synid_mappingdf$Id[database_synid_mappingdf$Database == "releaseFolder"]
+# For testing only
+# database_synid_mappingid = 'syn10967259'
+# genie_user=NULL
+# genie_pass=NULL
+# release = "9.3-consortium"
+# template_path = "genie/dashboardTemplate.Rmd"
+#
 
-choose_from_release = synTableQuery(paste(sprintf("select distinct(name) as releases from %s", release_folder_fileview_synid),"where name not like 'Release%' and name <> 'case_lists'"))
-releases = as.data.frame(choose_from_release)
-if (!any(releases$releases %in% release)) {
-  stop(sprintf("Must choose correct release: %s", paste0(releases$releases, collapse=", ")))
-}
+rmarkdown_path = sprintf('%s.Rmd', release)
+file.copy(template_path, rmarkdown_path, overwrite = T)
+rmarkdown::render(rmarkdown_path,
+                  params = list("genieUser" = genie_user,
+                                "geniePass" = genie_pass,
+                                "database_synid_mappingid" = database_synid_mappingid,
+                                "release" = release))
 
-#release_folder_fileview_synid = "syn17019650"
-release_folder = synTableQuery(sprintf("select id from %s where name = '%s'", release_folder_fileview_synid, release))
-release_folder_synid = release_folder$asDataFrame()$id
-template = readLines(template_path)
-
-rmarkdown_path = create_markdown(release, release_folder_synid, template)
-rmarkdown::render(rmarkdown_path,params = list("genieUser"=genie_user, "geniePass"=genie_pass))
 #Store the html file.
-release_html_ent = synStore(File(sprintf("%s.html",release),parentId=release_folder_synid))
-synStore(Wiki(markdown=sprintf("${preview?entityId=%s}",release_html_ent$properties$id),owner=release_folder_synid))
+release_html_ent = synStore(File(rmarkdown_path,
+                                 parentId = release_folder_synid))
+synStore(Wiki(markdown = sprintf("${preview?entityId=%s}",
+                                 release_html_ent$properties$id),
+              owner = release_folder_synid))
