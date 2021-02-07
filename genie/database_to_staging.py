2f8447ad5d0ecc66772ebd0ad2a23daaa9f0a824
Thomas Yu <thomas.yu@sagebase.org>
2021-02-07 10:32:28 +0800

    Update genie/database_to_staging.py

--- genie/database_to_staging.py
+++ genie/database_to_staging.py
@@ -266,7 +266,7 @@ def configure_maf(mafdf, remove_variants, flagged_variants):
     fillnas = ['t_depth', 't_ref_count', 't_alt_count',
                'n_depth', 'n_ref_count', 'n_alt_count']
     for col in fillnas:
-        mafdf[col][mafdf[col].astype(str) == "."]
+        mafdf[col][mafdf[col].astype(str) == "."] = float('nan')
     n_depth_ind = mafdf['n_depth'].astype(str).isin(["NA", "0.0", "0"])
     mafdf['Match_Norm_Seq_Allele2'][n_depth_ind] = ''
     mafdf['Match_Norm_Seq_Allele1'][n_depth_ind] = ''
