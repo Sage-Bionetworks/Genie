8406d08aa2f12037ab65dc6a1a5f554405bb1b99
thomasyu888 <thomas.yu@sagebase.org>
2022-04-28 15:15:52 +0800

    Add sample id validation to fusion file

--- tests/test_fusions.py
+++ tests/test_fusions.py
@@ -56,7 +56,8 @@ def test_processing(fusionClass):
         "HUGO_SYMBOL": ['AAED', 'AAK1', 'AAAS5'],
         "ENTREZ_GENE_ID": [0, 0, float('nan')],
         "CENTER": ["SAGE", "SAGE", "SAGE"],
-        "TUMOR_SAMPLE_BARCODE": ["ID1-1", "ID2-1", "ID1-3"],
+        "TUMOR_SAMPLE_BARCODE": ["GENIE-SAGE-ID1-1", "GENIE-SAGE-ID2-1",
+                                 "GENIE-SAGE-ID1-3"],
         "FUSION": ["AAED-AAK1", "AAAS-AAK1", "AAAS-AAK1"],
         "DNA_SUPPORT": ["foo", 'foo', 'foo'],
         "RNA_SUPPORT": ["foo", 'foo', 'foo'],
@@ -67,16 +68,19 @@ def test_processing(fusionClass):
     assert expectedFusionDf.equals(newFusionDf[expectedFusionDf.columns])
 
 
-def test_validation(fusionClass):
+def test_validation__fail_name(fusionClass):
     with pytest.raises(AssertionError):
         fusionClass.validateFilename(['foo'])
     assert fusionClass.validateFilename(["data_fusions_SAGE.txt"]) == "fusions"
 
+
+def test_validation_perfect(fusionClass):
     fusionDf = pd.DataFrame({
         "HUGO_SYMBOL": ['AAED', 'AAK1', 'AAAS'],
         "ENTREZ_GENE_ID": [0, 0, 0],
         "CENTER": ["SAGE", "SAGE", "SAGE"],
-        "TUMOR_SAMPLE_BARCODE": ["ID1-1", "ID2-1", "ID1-3"],
+        "TUMOR_SAMPLE_BARCODE": ["GENIE-SAGE-ID1-1", "GENIE-SAGE-ID2-1",
+                                 "GENIE-SAGE-ID1-3"],
         "FUSION": ["AAED-AAK1", "AAAS-AAK1", "AAAS-AAK1"],
         "DNA_SUPPORT": ["foo", 'foo', 'foo'],
         "RNA_SUPPORT": ["foo", 'foo', 'foo'],
@@ -87,6 +91,8 @@ def test_validation(fusionClass):
         assert error == ""
         assert warning == ""
 
+
+def test_validation_perfect_invalid(fusionClass):
     fusionDf = pd.DataFrame({
         "HUGO_SYMBOL": [float('nan'), 'AAK1', 'AAAS'],
         "CENTER": ["SAGE", "SAGE", "SAGE"],
@@ -100,7 +106,9 @@ def test_validation(fusionClass):
         error, warning = fusionClass._validate(fusionDf, False)
         expectedErrors = (
             "Your fusion file must at least have these headers: ENTREZ_GENE_ID.\n"
-            "Your fusion file should not have any NA/blank Hugo Symbols.\n")
+            "Your fusion file should not have any NA/blank Hugo Symbols.\n"
+            "fusion: TUMOR_SAMPLE_BARCODE must start with GENIE-SAGE\n"
+        )
 
         assert error == expectedErrors
         assert warning == ""
@@ -108,6 +116,8 @@ def test_validation(fusionClass):
     with patch.object(syn, "get", return_value=ENTITY):
         error, warning = fusionClass._validate(fusionDf, True)
         expectedErrors = (
-            "Your fusion file must at least have these headers: ENTREZ_GENE_ID.\n")
+            "Your fusion file must at least have these headers: ENTREZ_GENE_ID.\n"
+            "fusion: TUMOR_SAMPLE_BARCODE must start with GENIE-SAGE\n"
+        )
         assert error == expectedErrors
         assert warning == ""
