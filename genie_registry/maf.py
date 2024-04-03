21e1d54b0d2fd5f900be8a621c40e931d62ef92a
rxu17 <26471741+rxu17@users.noreply.github.com>
2024-04-02 17:39:54 -0700

    add visual ex

--- genie_registry/maf.py
+++ genie_registry/maf.py
@@ -25,8 +25,24 @@ def _check_allele_col_validity(df):
 
         TSA1 is used by Genome Nexus (GN) to annotate data when it senses there is ambiguity
         regarding which variant (TSA1 vs TSA2) to use. This is
-        why we cannot have rows where we have TSA1 == REF and rows
-        where we have TSA1 == TSA2.
+        why there cannot be mixed rows where some rows have TSA1 == REF and some rows
+        have TSA1 == TSA2.
+        
+        e.g:
+        VALID
+        | REFERENCE_ALLELE | TUMOR_SEQ_ALLELE1 | TUMOR_SEQ_ALLELE2
+        | C                | C                 | A
+        | T                | T                 | C
+        
+        VALID
+        | REFERENCE_ALLELE | TUMOR_SEQ_ALLELE1 | TUMOR_SEQ_ALLELE2
+        | C                | A                 | A
+        | T                | C                 | C
+        
+        INVALID
+        | REFERENCE_ALLELE | TUMOR_SEQ_ALLELE1 | TUMOR_SEQ_ALLELE2
+        | C                | C                 | A
+        | C                | A                 | A
 
         See https://github.com/genome-nexus/annotation-tools/issues/26 for
         more background regarding why this validation rule was implemented.
