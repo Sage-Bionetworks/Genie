c0bd5283496339f45f8666a05829b60ece0882a0
rxu17 <26471741+rxu17@users.noreply.github.com>
2025-08-15 14:51:55 -0700

    add more tests

--- tests/test_dashboard_table_updater.py
+++ tests/test_dashboard_table_updater.py
@@ -7,9 +7,10 @@ from genie import dashboard_table_updater as dash_update
     "input_string_time, expected_output_time",
     [
         ("2018-10-25T20:16:07.959Z", 1540498567000),
+        ("2018-10-25T20:16:07.959", 1540498567000),
         ("2018-04-06T18:30:00", 1523039400000),
     ],
-    ids=["utc_time", "local_time_zone"],
+    ids=["utc_time", "local_time_zone", "no_milliseconds"],
 )
 def test_that_string_to_unix_epoch_time_milliseconds_gives_expected_time(
     input_string_time, expected_output_time
